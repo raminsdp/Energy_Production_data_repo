09148f90c6cfae5e979ff12f78934454985f3207
Ullrich Seidel <ulli.seidel@gmail.com>
2024-03-21 11:17:30 +0100

    updated files

--- Jupyter_Notebooks/data_gathering.py
+++ Jupyter_Notebooks/data_gathering.py
@@ -1,11 +1,12 @@
 # files that needed to be downloaded before running the script:
 # data source: https://netztransparenz.tennet.eu/de/strommarkt/transparenz/transparenz-deutschland/netzkennzahlen/tatsaechliche-und-prognostizierte-solarenergieeinspeisung/bayern/
 # manually filtered for 2023-01-01 to 2023-12-31, output will be named 'solarEnergyFeedIn_BY_2023-01-01_2023-12-31.csv'
+# the file have to be saved in Dataset/raw_data/ (folder has to be created)
 #
 # data source: http://www.marktstammdatenregister.de/MaStR/Einheit/Einheiten/OeffentlicheEinheitenuebersicht?filter=Inbetriebnahmedatum%20der%20Einheit~lt~%2701.01.2024%27~and~Inbetriebnahmedatum%20der%20Einheit~gt~%2731.12.2014%27~and~Bundesland~eq~%271403%27~and~Energietr%C3%A4ger~eq~%272495%27
 # filters for Bavaria, solar energy and targeted timeframe already applied in the link
 # 465169 data points in 94 csv files, manually downloaded on March 08, 2024 (daily changes to data occur)
-# 
+# the files have to be saved in Dataset/raw_data/
 
 import pandas as pd
 import os
@@ -44,12 +45,15 @@ for station in stations:
     df_stat.columns =['date',station+'_FD_STRAHL',station+'_FG_STRAHL',station+'_SD_STRAHL']
     df_stations = df_stations.merge(df_stat.loc[mask,:],on='date')
 
+
 ### production of solar energy in Bavaria in megawatt
 df_en_raw = pd.read_table('../Dataset/raw_data/solarEnergyFeedIn_BY_2023-01-01_2023-12-31.csv',sep=';',parse_dates=['Datum'],decimal=',')
 
+# create empty dataframe based on targeted dates
 df_en = pd.DataFrame({'date': pd.date_range(start='1/1/2023', freq='1d', periods=365)})
 df_en.loc[:,'Prog_in_MW'] = pd.DataFrame(df_en_raw.groupby(['Datum'], as_index=False)['Prognostiziert in MW'].sum()).iloc[:,1]
 df_en.loc[:,'Act_in_MW'] = pd.DataFrame(df_en_raw.groupby(['Datum'], as_index=False)['Tatsaechlich in MW'].sum()).iloc[:,1]
+
 
 ### registered solar modules (MWp)
 def read_and_combine_files(start, end, folder_path):
@@ -61,18 +65,18 @@ def read_and_combine_files(start, end, folder_path):
         # Erstelle den Dateinamen basierend auf dem Nummernbereich
         file_name = f"Stromerzeuger_{i}_bis_{i + 4999}.csv" 
 
-        # Erstelle den vollständigen Pfad zur CSV-Datei
+        # create path to file
         file_path = os.path.join(folder_path, file_name)
 
-        # Lese das CSV in einen DataFrame ein und füge ihn zur Liste hinzu
+        # read csv and append it to dataframe
         df = pd.read_csv(file_path, delimiter=';')
         dfs.append(df)
 
-    # Lese die letzte Datei ein
-    last_file_path = os.path.join(folder_path, 'Stromerzeuger_465001_bis_465169.csv')
+    # read in of last file, name has to be adjusted 
+    last_file_path = os.path.join(folder_path, 'Stromerzeuger_465001_bis_465169.csv') # highest number has to be adjusted
     last_df = pd.read_csv(last_file_path, delimiter=';')
 
-    # Füge die letzte Datei zum DataFrame hinzu
+    # append data to dataframe
     dfs.append(last_df)
 
     # Kombiniere alle DataFrames nach dem Index
@@ -86,31 +90,31 @@ combined_df = read_and_combine_files(1, 464999, folder_path)
 # creating empty Dataframe
 df_modules = pd.DataFrame({'date': pd.date_range(start='1/1/2015', end='12/31/2023', freq='1d')})
 
-# Nur die Zeilen filtern, die den 'Betriebs-Status' 'In Betrieb' haben
+# masking data with 'Betriebs-Status' 'In Betrieb' (actual in operation)
 mask_combined_df = (combined_df['Betriebs-Status'] == 'In Betrieb')
-df_combined_filtered = combined_df.loc[mask_combined_df, :].copy()  # Kopie erstellen
+df_combined_filtered = combined_df.loc[mask_combined_df, :].copy()  
 
-# 'Inbetriebnahmedatum der Einheit' in Datumstyp konvertieren
+# 'Inbetriebnahmedatum der Einheit' converting to datetime
 df_combined_filtered['Inbetriebnahmedatum der Einheit'] = pd.to_datetime(df_combined_filtered['Inbetriebnahmedatum der Einheit'], format="%d.%m.%Y", errors='coerce')
 
-# Gruppensummen für 'Bruttoleistung der Einheit' und 'Nettonennleistung der Einheit' basierend auf 'Inbetriebnahmedatum der Einheit' berechnen
+# calculating grouped sum of 'Bruttoleistung der Einheit' and 'Nettonennleistung der Einheit' based on 'Inbetriebnahmedatum der Einheit' 
 grouped_sum = df_combined_filtered.groupby('Inbetriebnahmedatum der Einheit').agg({
     'Bruttoleistung der Einheit': lambda x: x.str.replace(',', '.').astype(float).sum(),
     'Nettonennleistung der Einheit': lambda x: x.str.replace(',', '.').astype(float).sum()
 }).reset_index()
 
-# Spaltennamen ändern
+# rename columns
 grouped_sum = grouped_sum.rename(columns={'Bruttoleistung der Einheit': 'Bruttoleistung', 'Nettonennleistung der Einheit': 'Nettoleistung'})
 
-# Gruppensummen in den DataFrame df_combined einfügen
+# merge grouped sums on date
 df_modules = df_modules.merge(grouped_sum, left_on='date', right_on='Inbetriebnahmedatum der Einheit', how='left').fillna(0)
 
-# Ergebnisse anzeigen
+# drop unnecessary column
 df_modules = df_modules.drop(columns=['Inbetriebnahmedatum der Einheit'])
 
-## alternatively collecting data from 1 downloaded file:
+## alternatively collecting data from 1 downloaded file: ##
 ## data source: https://download.marktstammdatenregister.de/Gesamtdatenexport_20240306_23.2.zip (or any current version from 'Gesamtdatenauszug vom Vortag' at https://www.marktstammdatenregister.de/MaStR/Datendownload)
-## files: EinheitenSolar_1-39.xml, manually unpacked in /Dataset
+## files: EinheitenSolar_1-39.xml, manually unpacked in /Dataset/raw_data
 ## script can be applied to any state and/or timeframe, but will take several hours for a run
 #
 #import xml.etree.ElementTree as ET
@@ -145,7 +149,7 @@ df_modules = df_modules.drop(columns=['Inbetriebnahmedatum der Einheit'])
 #                df_mod_raw.loc[counter] = [datum,plz,brutto,netto,inbetrieb]
 #                counter = counter + 1
 
-# merging dataframes
+### merging dataframes
 df_final_raw_2015_2023 = df_modules.merge(df_modules,df_en, on = 'date')
 df_final_raw_2015_2023 = df_final_raw_2015_2023.merge(df_stations, on = 'date')
 df_final_raw_2015_2023.to_csv('../Dataset/preprocessed_data/df_final_raw_2015_2023.csv')
